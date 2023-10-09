60299e60d497b4e09a04d084bb80e133bdd38adb
doerglia <llammoglia@gmail.com>
2023-10-08 23:46:46 -0300

    Repositorio del trabajo final de la segunda entrega del curso TICMAS

---
+++ js/app.js
@@ -0,0 +1,96 @@
+//Menu lateral
+var menu_visible = false;
+let menu = document.getElementById("nav");
+function mostrarOcultarMenu(){
+    if(menu_visible==false){//si esta oculto
+        menu.style.display = "block";
+        menu_visible = true;
+    }
+    else{
+        menu.style.display = "none";
+        menu_visible = false;
+    }
+}
+//oculto el menu una vez que selecciono una opción
+let links = document.querySelectorAll("nav a");
+for(var x = 0; x <links.length;x++){
+    links[x].onclick = function(){
+        menu.style.display = "none";
+        menu_visible = false;
+    }
+}
+
+//Creo las barritas de una barra particular identificada por su id
+function crearBarra(id_barra){
+    for(i=0;i<=16;i++){
+        let div = document.createElement("div");
+        div.className = "e";
+        id_barra.appendChild(div);
+    }
+}
+
+//selecciono todas las barras generales para luego manipularlas
+let html = document.getElementById("html");
+crearBarra(html);
+let javascript = document.getElementById("javascript");
+crearBarra(javascript);
+let powerbi = document.getElementById("powerbi");
+crearBarra(powerbi);
+let sql = document.getElementById("sql");
+crearBarra(sql);
+let platform = document.getElementById("platform");
+crearBarra(platform);
+let python = document.getElementById("python");
+crearBarra(python);
+
+//Se guarda la cantidad de barritas que se van a ir pintando por cada barar
+//Uso un arreglo, cada posiciòn pertenece a un elemento
+//Comienza en -1 porque no tiene ninguna pintada al iniciarse
+let contadores = [-1,-1,-1,-1,-1,-1];
+
+//esta variable la uso de bandera para saber si ya ejecuto la animación
+let entro = false;
+
+//función que aplica las animaciones de la habilidades
+function efectoHabilidades(){
+    var habilidades = document.getElementById("habilidades");
+    var distancia_skills = window.innerHeight - habilidades.getBoundingClientRect().top;
+    if(distancia_skills>=300 && entro==false){
+        entro = true;
+        const intervalHtml = setInterval(function(){
+            pintarBarra(html, 14, 0, intervalHtml);
+        },100);
+        const intervalJavascript = setInterval(function(){
+            pintarBarra(javascript, 11, 1, intervalJavascript);
+        },100);
+        const intervalpowerbi = setInterval(function(){
+            pintarBarra(powerbi, 16, 2, intervalpowerbi);
+        },100);
+        const intervalsql = setInterval(function(){
+            pintarBarra(sql, 15, 3, intervalsql);
+        },100);
+        const intervalplatform = setInterval(function(){
+            pintarBarra(platform, 16, 4, intervalplatform);
+        },100);
+        const intervalpython = setInterval(function(){
+            pintarBarra(python, 11, 5, intervalpython);
+        },100);
+    }
+}
+
+//lleno una barra particular con la cantidad indicada
+function pintarBarra(id_barra, cantidad, indice, interval){
+    contadores[indice]++;
+    x = contadores[indice];
+    if(x < cantidad){
+        let elementos = id_barra.getElementsByClassName("e");
+        elementos[x].style.backgroundColor = "#940253";
+    }else{
+        clearInterval(interval)
+    }
+}
+
+//detecto el scrolling del mouse para aplicar la animación de la barra
+window.onscroll = function(){
+    efectoHabilidades();
+}
